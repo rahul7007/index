1e51e142109b0e0c0f07f3809ad769fb0f48bea6
rahul-sarma <rahul.sarma@hp.com>
2021-01-30 18:54:04 +0530

    added build version

---
+++ Frontend/build/precache-manifest.78fdff55f4ef601fc117867a95893c29.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bf8129dd47c3992df1f413b93d40a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d7c7c3f030c15f52dc8",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "87130b05609d93f3d96c",
+    "url": "/static/css/main.7a74d8bb.chunk.css"
+  },
+  {
+    "revision": "2d7c7c3f030c15f52dc8",
+    "url": "/static/js/2.64807cb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.64807cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87130b05609d93f3d96c",
+    "url": "/static/js/main.4988bc66.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "2e532e685360fe9b50341d656c624f12",
+    "url": "/static/media/about.2e532e68.png"
+  },
+  {
+    "revision": "f85ddbeb6c395a1279aba30914693522",
+    "url": "/static/media/myavatar.f85ddbeb.png"
+  },
+  {
+    "revision": "e197fe88a4805f81ed01431b1c2f9574",
+    "url": "/static/media/no-information.e197fe88.jpg"
+  },
+  {
+    "revision": "8b31d90d857452294a5b2cc34fee6e2f",
+    "url": "/static/media/work-4.8b31d90d.gif"
+  },
+  {
+    "revision": "58ea51fc0a721a766c36b4da6a24210a",
+    "url": "/static/media/work1.58ea51fc.jpg"
+  },
+  {
+    "revision": "394d884ba6cf50d34053fbdc8405e6ee",
+    "url": "/static/media/work21.394d884b.jpg"
+  },
+  {
+    "revision": "cca70481d3d3cc3a60434dcfcb481703",
+    "url": "/static/media/work3.cca70481.jpg"
+  }
+]);
